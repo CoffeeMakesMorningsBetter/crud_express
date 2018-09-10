9df2b363761a77677b2a811bc05aa4c804cdce1b
Mark Suzuki <markdsuzuki@gmail.com>
2018-09-09 22:07:34 -0700

    middleware for errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const itemRoutes = require('./routes')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -9,8 +10,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/items',itemRoutes)
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   return res.json("Start with /items");
+});
+
+app.use((req, res, next) => {
+  const err = new Error("404 Not Found")
+  err.status = 404
+  return next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {}
+  });
 });
 
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,33 @@ const router = express.Router()
 const items = [{itemName: 'chips', id: 0}]
 let id = 1
 
-router.get("", (req, res) => {
+router.get("", (req, res, next) => {
   return res.json(items)
 })
 
-router.post("", (req, res) => {
+router.post("", (req, res, next) => {
   users.push({
     itemName: req.body.itemName,
     id: ++id
   })
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
   const singleItem = items.find(item => item.id === Number(req.params.id))
+  if(!singleItem) {
+    return next(err)
+  }
   console.log(singleItem)
   return res.json(singleItem)
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
   const singleItem = items.find(item => item.id === Number(req.params.id))
   singleItem.itemName = req.body.itemName
   return res.json({ message: "Updated" })
 })
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res, next) => {
   const removeItem = items.findIndex(item => item.id === Number(req.params.id))
   items.slice(removeItem,1)
   return res.json({ message: "Deleted" });
